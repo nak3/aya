c90ffdb9bc222c04d1bea931f6b3a394e7974967
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2022-11-15 15:15:29 +0900

    wip

--- aya/src/programs/lwt_in.rs
+++ aya/src/programs/lwt_in.rs
@@ -8,6 +8,24 @@ use crate::{
     },
 };
 
+use crate::programs::RawFd;
+
+
+use crate::{
+    generated::{
+        bpf_attach_type::{self, BPF_LWT_IN},
+        bpf_link_type,
+    },
+    programs::{
+        define_link_wrapper, load_program, FdLink, Link, LinkError, ProgramData, ProgramError,
+    },
+    sys::{
+        bpf_link_create, bpf_link_get_info_by_fd, bpf_link_update, kernel_version,
+        netlink_set_xdp_fd,
+    },
+};
+
+
 /// A program that attaches to Linux LSM hooks. Used to implement security policy and
 /// audit logging.
 ///
@@ -48,8 +66,24 @@ use crate::{
 #[derive(Debug)]
 #[doc(alias = "BPF_PROG_TYPE_LWT_IN")]
 pub struct LwtIn {
-    pub(crate) data: ProgramData<LsmLink>,
+    pub(crate) data: ProgramData<LwtInLink>,
 }
+
+/// Errors from LWT_IN programs
+#[derive(Debug, Error)]
+pub enum LwtInError {
+    /// netlink error while attaching ebpf program
+    #[error("netlink error while attaching ebpf program to tc")]
+    NetlinkError {
+        /// the [`io::Error`] from the netlink call
+        #[source]
+        io_error: io::Error,
+    },
+    /// the clsact qdisc is already attached
+    #[error("the clsact qdisc is already attached")]
+    AlreadyAttached,
+}
+
 
 impl LwtIn {
     /// Loads the program inside the kernel.
@@ -65,14 +99,40 @@ impl LwtIn {
     /// Attaches the program.
     ///
     /// The returned value can be used to detach, see [Lsm::detach].
-    pub fn attach(&mut self) -> Result<LsmLinkId, ProgramError> {
-        attach_raw_tracepoint(&mut self.data, None)
+    pub fn attach(
+        &mut self,
+        interface: &str,
+        attach_type: LwtInAttachType,
+        priority: u16,
+    ) -> Result<LwtInLinkId, ProgramError> {
+        let prog_fd = self.data.fd_or_err()?;
+        let c_interface = CString::new(interface).unwrap();
+        let if_index = ifindex_from_ifname(interface)
+            .map_err(|io_error| LwtInError::NetlinkError { io_error })?;
+
+        if if_index == 0 {
+            return Err(ProgramError::UnknownInterface {
+                name: interface.to_string(),
+            });
+        }
+
+        let k_ver = kernel_version().unwrap();
+
+            let link_fd = bpf_link_create(prog_fd, if_index, BPF_LWT_IN, None, flags.bits).map_err(
+                |(_, io_error)| ProgramError::SyscallError {
+                    call: "bpf_link_create".to_owned(),
+                    io_error,
+                },
+            )? as RawFd;
+            self.data
+                .links
+                .insert(LwtInLink(LwtInLinkInner::FdLink(FdLink::new(link_fd))))
     }
 
     /// Detaches the program.
     ///
-    /// See [Lsm::attach].
-    pub fn detach(&mut self, link_id: LsmLinkId) -> Result<(), ProgramError> {
+    /// See [LwtIn::attach].
+    pub fn detach(&mut self, link_id: LwtInLinkId) -> Result<(), ProgramError> {
         self.data.links.remove(link_id)
     }
 
@@ -80,16 +140,16 @@ impl LwtIn {
     ///
     /// The link will be detached on `Drop` and the caller is now responsible
     /// for managing its lifetime.
-    pub fn take_link(&mut self, link_id: LsmLinkId) -> Result<LsmLink, ProgramError> {
+    pub fn take_link(&mut self, link_id: LwtInLinkId) -> Result<LwtInLink, ProgramError> {
         self.data.take_link(link_id)
     }
 }
 
 define_link_wrapper!(
     /// The link used by [Lsm] programs.
-    LsmLink,
+    LwtInLink,
     /// The type returned by [Lsm::attach]. Can be passed to [Lsm::detach].
-    LsmLinkId,
+    LwtInLinkId,
     FdLink,
     FdLinkId
 );
